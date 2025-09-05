7cb92ce3e7ca00a10934ca92f9f3489bb942ff72
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2025-09-05 17:45:14 +0900

    first commit

---
+++ composeApp/src/commonMain/kotlin/com/fujikouki/anical/Greeting.kt
@@ -0,0 +1,9 @@
+package com.fujikouki.anical
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/com/fujikouki/anical/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.fujikouki.anical
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
